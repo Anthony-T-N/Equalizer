1eae70320669ecbdf3d7b6e82af065c9f49c4dc6
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2020-03-14 16:00:32 +1030

    Update Equalizer.java

--- Equalizer.java
+++ Equalizer.java
@@ -7,6 +7,14 @@ public class Equalizer
 		Random randomGenerator = new Random();
 		boolean dialswitch = true;
 		int tries = 0;
+		while True
+		{
+			int key1 = randomGenerator.nextInt(100);
+			int key2 = randomGenerator.nextInt(100);
+			int key3 = randomGenerator.nextInt(100);
+			System.out.println(key1+" "+key2+" "+key3);
+			tries++;
+		}
   	}	
   
 }
